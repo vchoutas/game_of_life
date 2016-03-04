a0b09b01334a9add4737039a19791b43cbd58377
vasilish <vasilis4ch@gmail.com>
2016-03-04 23:23:03 +0200

    Remove unused struct in utilities header

--- include/utilities.h
+++ include/utilities.h
@@ -14,35 +14,13 @@
 #define SWAP(x, y) do {typeof(x) SWAP = x; x = y; y = SWAP; } while(0)
 
 
-
 namespace utilities
 {
   void read_from_file(bool *X, std::string filename, size_t N);
   void generate_table(bool *X, size_t N);
   void save_table(int *X, int N);
 
-  // struct color
-  // {
-    // public:
-      // GLubyte red;
-      // GLubyte green;
-      // GLubyte blue;
-
-      // color()
-      // {
-        // red = 0;
-        // green = 0;
-        // blue = 0;
-      // }
-      // color(GLubyte r, GLubyte g, GLubyte b)
-      // {
-        // red = r;
-        // green = g;
-        // blue = b;
-      // }
-  // };
 } // namespace utilities
 
-// typedef utilities::color color;
 
 #endif
